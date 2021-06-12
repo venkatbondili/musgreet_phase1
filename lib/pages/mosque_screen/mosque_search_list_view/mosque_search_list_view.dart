2bfee5cdc2052e53926a3e779c6f1749379bf4d0
Sindhuja67 <Sindhujamohan67@gmail.com>
2021-06-12 11:56:04 +0100

    Mosque Search list view and advanced search functionality added

--- lib/pages/mosque_screen/mosque_search_list_view/mosque_search_list_view.dart
+++ lib/pages/mosque_screen/mosque_search_list_view/mosque_search_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
 import 'package:mus_greet/core/config/navigation.dart';
 import 'package:mus_greet/core/utils/constants.dart';
@@ -7,10 +8,11 @@ import 'package:mus_greet/core/widgets/asset_image_widget.dart';
 import 'package:mus_greet/core/widgets/custom_spacer_widget.dart';
 import 'package:mus_greet/core/widgets/mosque_list_card_widget.dart';
 import 'package:mus_greet/core/widgets/search_text_field_widget.dart';
+import 'package:mus_greet/models/ModelProvider.dart';
+import 'package:mus_greet/models/Mosque.dart';
 
 class MosqueSearchListView extends StatefulWidget {
   final Function callBack;
-
   const MosqueSearchListView({Key key, this.callBack}) : super(key: key);
 
   @override
@@ -18,8 +20,37 @@ class MosqueSearchListView extends StatefulWidget {
 }
 
 class _MosqueSearchListViewState extends State<MosqueSearchListView> {
+  ArgumentClass args;
+  List<String> advancedSearchSectList = [];
+  List<String> advancedSearchFacilitiesList =[];
+  List<Mosque> Mosques = [];
+  List<Mosque> SearchedMosques = [];
+  List<Mosque> ResultMosques = [];
+  List<Facilitiesmaster> Facilitiesmasters = [];
+  List<Mosque> sectFilteredMosques = [];
+  List<Mosque> facilityFilteredMosques = [];
   @override
   Widget build(BuildContext context) {
+    print("printing args");
+    args = ModalRoute.of(context).settings.arguments as ArgumentClass;
+    print(args);
+    //listMosque(advancedSearchSectList,advancedSearchFacilitiesList);
+    if (args != null) {
+      advancedSearchSectList = args.advancedSearchSectList;
+      advancedSearchFacilitiesList = args.advancedSearchFacilitiesList;
+      print(advancedSearchSectList);
+      print(advancedSearchFacilitiesList);
+      //advancedSearchMosqueList(advancedSearchSectList, advancedSearchFacilitiesList);
+      listMosque(advancedSearchSectList, advancedSearchFacilitiesList, true);
+      //advancedSearchMosqueList(advancedSearchSectList,advancedSearchFacilitiesList);
+    }
+    else {
+      print(advancedSearchFacilitiesList);
+      print(advancedSearchSectList);
+      listMosque(advancedSearchSectList, advancedSearchFacilitiesList, false);
+    }
+    //advancedSearchSectList = args.advancedSearchSectList;
+    //advancedSearchFacilitiesList = args.advancedSearchFacilitiesList;
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.white,
@@ -35,7 +66,10 @@ class _MosqueSearchListViewState extends State<MosqueSearchListView> {
       backgroundColor: AppColors.white,
       automaticallyImplyLeading: false,
       bottom: PreferredSize(
-        preferredSize: Size(MediaQuery.of(context).size.width, 150),
+        preferredSize: Size(MediaQuery
+            .of(context)
+            .size
+            .width, 150),
         child: Container(
             padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
             child: Column(
@@ -57,12 +91,17 @@ class _MosqueSearchListViewState extends State<MosqueSearchListView> {
   }
 
   _getRowHeader() {
+    print("inside row header");
+    print(widget.callBack);
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       mainAxisSize: MainAxisSize.max,
       children: [
         GestureDetector(
-          onTap: ()=> widget.callBack(),
+          onTap: () {
+            _navigateback();
+            //widget.callBack();
+            },
           child: AssetImageWidget(
             image: ImageConstants.IC_BACK,
             height: 18,
@@ -81,7 +120,7 @@ class _MosqueSearchListViewState extends State<MosqueSearchListView> {
           ),
         ),
         GestureDetector(
-          onTap: ()=> Navigation.intent(context, AppRoutes.MOSQUE_FROM_MAP),
+          onTap: () => Navigation.intent(context, AppRoutes.MOSQUE_FROM_MAP),
           child: Container(
             height: 30,
             width: 30,
@@ -101,7 +140,7 @@ class _MosqueSearchListViewState extends State<MosqueSearchListView> {
 
   _getAdvanceSearchField() {
     return AdvanceSearchWidget(
-        callBack:() => _navigateToAdvanceSearchScreen()
+        callBack: () => _navigateToAdvanceSearchScreen()
     );
   }
 
@@ -112,7 +151,8 @@ class _MosqueSearchListViewState extends State<MosqueSearchListView> {
       child: ListView.separated(
         padding: EdgeInsets.only(top: 20, left: 15, right: 15, bottom: 20),
         itemBuilder: (context, index) {
-          return MosqueListCardWidget(index: index, );
+          return MosqueListCardWidget(
+              index: index, mosqueObject: ResultMosques[index]);
         },
         separatorBuilder: (context, index) {
           return Divider(
@@ -120,13 +160,212 @@ class _MosqueSearchListViewState extends State<MosqueSearchListView> {
             color: Colors.transparent,
           );
         },
-        itemCount: 10,
+        itemCount: ResultMosques.length,
       ),
     );
   }
 
 
-  _navigateToAdvanceSearchScreen(){
+  _navigateToAdvanceSearchScreen() {
     Navigation.intent(context, AppRoutes.MOSQUE_ADVANCE_SEARCH);
   }
+/// this method is to get result mosques list
+  Future<void> listMosque(List<String> advancedSearchSectList,List<String> advancedSearchFacilitiesList, bool  advanceSearchStatus) async {
+    print("inside list mosques");
+
+    ///get the master facilitieslist
+    try {
+      Facilitiesmasters =
+      await Amplify.DataStore.query(Facilitiesmaster.classType);
+      print(Facilitiesmasters);
+    } catch (e) {
+      print("Could not query DataStore: " + e);
+    }
+
+    ///get the list of mosques
+    try {
+      Mosques = await Amplify.DataStore.query(Mosque.classType);
+      print("Mosque length");
+      print(Mosques.length);
+    } catch (e) {
+      print("Could not query DataStore: " + e);
+    }
+    if (advanceSearchStatus == true){
+      await Future.delayed(Duration(seconds: 1));
+      if(Mosques != []){
+        print(advancedSearchSectList);
+      //advancedSearchMosqueList(advancedSearchSectList,advancedSearchFacilitiesList);
+        advancedSearchSectFilter(advancedSearchSectList, advancedSearchFacilitiesList);
+      }
+    }
+    else{
+      await Future.delayed(Duration(seconds: 1));
+      print("inside else block in list Mosques method");
+      ResultMosques = Mosques;
+      print("ResultMosques");
+      print(ResultMosques.length);
+      print(ResultMosques);
+    }
+
+  }
+/// this method is not in use noe
+  Future<void> advancedSearchMosqueList(List<String> advancedSearchSectList, List<String> advancedSearchFacilitiesList) async {
+    print("inside advanced Search");
+    print(Mosques);
+    SearchedMosques = [];
+    ResultMosques = [];
+    //Facilitiesmasters = [];
+    print(advancedSearchSectList);
+    print(advancedSearchFacilitiesList);
+    if (advancedSearchFacilitiesList == [] && advancedSearchSectList == []) {
+      SearchedMosques= Mosques;
+      ResultMosques = SearchedMosques;
+      print("normal search result SearchedMosques Length");
+      print(ResultMosques.length);
+    }
+    else {
+      ///condition to filter mosques based on sect
+      print("going for sect filter");
+      print(advancedSearchSectList);
+      if(advancedSearchSectList.isEmpty){
+        print("when sect is empty");
+        print("MosquesLength");
+        print(Mosques.length);
+        SearchedMosques = Mosques;
+        ResultMosques = SearchedMosques;
+        print(Mosques.length);
+      }
+      else{
+        print("inside sect else");
+        for (var sect in advancedSearchSectList) {
+          for (var i in Mosques) {
+            if (sect.toLowerCase() == i.sect.toLowerCase()) {
+              print(sect.toLowerCase());
+              print(i.sect.toLowerCase());
+              SearchedMosques.add(i);
+            }
+          }
+        }
+        print("after sect filtered");
+        print(SearchedMosques.length);
+        ResultMosques=SearchedMosques;
+        if(ResultMosques.length == 0){print("no result available");}
+      }
+      print("Starting facilities filter");
+      ///condition to filter based on facilities list
+      print(advancedSearchFacilitiesList);
+      //print(Facilitiesmasters[0]);
+      print("Searched mosques length");
+      print(SearchedMosques.length);
+      print("result mosques length");
+      print(ResultMosques.length);
+      if (advancedSearchFacilitiesList.isNotEmpty && Facilitiesmasters.isNotEmpty) {
+        print("SearchedMosques length loop inside ");
+        print(SearchedMosques.length);
+        for (var j in SearchedMosques) {
+          List<String> MosqueFacilitiesList = j.mosque_facility_list.split(',');
+          //print(MosqueFacilitiesList);
+          for (var id in MosqueFacilitiesList) {
+            for (var masterfacility in Facilitiesmasters) {
+              if (id.toLowerCase() == masterfacility.id.toLowerCase()) {
+                for(var searchFacility in advancedSearchFacilitiesList){
+                  if(searchFacility.toLowerCase() == masterfacility.facility_header.toLowerCase()){
+                    print(searchFacility.toLowerCase());
+                    ResultMosques.add(j);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    print("Final result list");
+    print(ResultMosques.length);
+    print(ResultMosques);
+  }
+
+///this method is to get advanced search filtered mosque list
+  Future<void> advancedSearchSectFilter(List<String> advancedSearchSectList, List<String> advancedSearchFacilitiesList) async {
+    bool Status = false;
+    sectFilteredMosques = [];
+    if (Mosques.isNotEmpty && advancedSearchSectList.isNotEmpty){
+      for (var s in advancedSearchSectList){
+        for(var m in Mosques){
+          if(s.toLowerCase() == m.sect.toLowerCase()){
+            if(advancedSearchFacilitiesList.isNotEmpty){
+            Status = advancedSearchFacilityFilter(m, advancedSearchFacilitiesList);
+            }
+            else{
+              Status = true;
+            }
+            if(Status == true){
+              sectFilteredMosques.add(m);
+            }
+          }
+        }
+      }
+    }
+    else {
+      //sectFilteredMosques = Mosques;
+      for(var m in Mosques){
+        Status = advancedSearchFacilityFilter(m, advancedSearchFacilitiesList);
+        if(Status == true){
+          sectFilteredMosques.add(m);
+        }
+      }
+    }
+    ResultMosques = sectFilteredMosques;
+    print(ResultMosques.length);
+    print(ResultMosques);
+
+  }
+
+  ///This method is to get filtered mosques list by facilities
+  advancedSearchFacilityFilter(Mosque filteredMosque, List<String> advancedSearchFacilitiesList){
+    bool status = false;
+    int count = 0;
+    List<String> mosqueFacilityList = [];
+    if (filteredMosque != null && advancedSearchFacilitiesList.isNotEmpty){
+      mosqueFacilityList = filteredMosque.mosque_facility_list.split(',');
+      for (var facilityName in advancedSearchFacilitiesList){
+        for(var facilityId in mosqueFacilityList){
+          for(var masterFacility in Facilitiesmasters){
+            if(facilityId.toLowerCase() == masterFacility.id.toLowerCase()){
+              if(masterFacility.facility_header.toLowerCase() == facilityName.toLowerCase()){
+                count ++;
+              }
+            }
+          }
+        }
+      }
+      if(count > 0){
+        status = true;
+      }
+    }
+    else{
+      //ResultMosques = Mosques;
+      status = false;
+    }
+    return status;
+  }
+
+  _navigateback() {
+    if(widget.callBack == null){
+      Navigation.intent(context, AppRoutes.HOME);
+    }else {
+      print(widget.callBack);
+      Navigation.intent(context, AppRoutes.HOME);
+      //widget.callBack;
+    }
+  }
 }
+
+class ArgumentClass {
+  final List<String> advancedSearchSectList;
+  final List<String> advancedSearchFacilitiesList;
+  ArgumentClass(this.advancedSearchSectList, this.advancedSearchFacilitiesList);
+
+}
+
